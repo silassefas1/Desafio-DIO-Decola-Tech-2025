89e2ebdefa363bb773f111ba524a5fd963e59c6f
Silas Sefas <silassefas23@gmail.com>
2025-02-26 17:26:18 -0300

    feat(product): Product Model Created

---
+++ src/main/java/com/silassefas/Desafio_DIO_Decola_Tech_2025/model/Product.java
@@ -0,0 +1,43 @@
+package com.silassefas.Desafio_DIO_Decola_Tech_2025.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "elite_product")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String category;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    
+    
+
+}
