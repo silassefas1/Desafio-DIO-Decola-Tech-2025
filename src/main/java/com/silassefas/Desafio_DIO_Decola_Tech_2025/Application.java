234de65cfabb268d3f328abb2ef5d9797ecd518d
Silas Sefas <silassefas23@gmail.com>
2025-02-26 13:59:22 -0300

    Initial Commit: Creating Project

---
+++ src/main/java/com/silassefas/Desafio_DIO_Decola_Tech_2025/Application.java
@@ -0,0 +1,13 @@
+package com.silassefas.Desafio_DIO_Decola_Tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
