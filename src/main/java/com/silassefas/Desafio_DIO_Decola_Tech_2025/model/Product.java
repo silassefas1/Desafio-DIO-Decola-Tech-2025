43d92d11f5e259dd03f3d34c2abc91c8ab23bc68
Silas Sefas <silassefas23@gmail.com>
2025-03-01 09:42:02 -0300

    fix(product): removing duplicated import @Id

--- src/main/java/com/silassefas/Desafio_DIO_Decola_Tech_2025/model/Product.java
+++ src/main/java/com/silassefas/Desafio_DIO_Decola_Tech_2025/model/Product.java
@@ -3,7 +3,7 @@ package com.silassefas.Desafio_DIO_Decola_Tech_2025.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
